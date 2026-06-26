6ed17c346413f48576e08fea417ae7c64a042781
1365875125-design <1365875125@qq.com>
2026-06-26 17:54:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,31 @@
-const CACHE_NAME = 'growthos-prd-v1';
+const CACHE_NAME = 'growthos-demo-app-v2';
+
 const ASSETS = [
   './',
   './index.html',
-  './growthos-prd.html',
-  './icon.svg',
   './manifest.json',
-  './_shared/js/echarts.min.js',
-  './_shared/js/mermaid.min.js'
+  './icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
   );
+
   self.clients.claim();
 });
 
@@ -33,16 +33,20 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+
+      return fetch(event.request)
+        .then((response) => {
+          if (!response || response.status !== 200) return response;
+
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, copy);
+          });
+
           return response;
-        }
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      });
+        })
+        .catch(() => caches.match('./index.html'));
     })
   );
 });
